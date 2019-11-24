a4ac110b6564a08a3b9794f3825763119c43a1a6
Xiaohan <767192049@qq.com>
2019-11-24 12:25:59 +0800

    add the nodes and links

--- index.js
+++ index.js
@@ -1,5 +1,59 @@
 import Chart from "./chart.js";
+let tmp = [
+    {"id": "California"},{"id": "Maine"},{"id": "Arizona"},
+    {"id": "New York"},{"id": "North Dakota"},{"id": "Texas"},
+    {"id": "Montana"},{"id": "Minnesota"},{"id": "Vermont"},
+    {"id": "Washington"},{"id": "Idaho"},{"id": "Michigan"},
+    {"id": "New Mexico"},{"id": "Alaska"},{"id": "Ohio"},
+    {"id": "Calexico East"},{"id": "Van Buren"},{"id": "Otay Mesa"},
+    {"id": "Nogales"},{"id": "Trout River"},{"id": "Madawaska"},
+    {"id": "Lancaster"},{"id": "Progreso"},{"id": "Portal"},
+    {"id": "Champlain-Rouses Point"},{"id": "Opheim"},{"id": "Neche"},
+    {"id": "Derby Line"},{"id": "Sarles"},{"id": "Wildhorse"},
+    {"id": "Lynden"},{"id": "Vanceboro"},{"id": "San Ysidro"},
+    {"id": "Scobey"},{"id": "Beecher Falls"},{"id": "Calais"},
+    {"id": "Massena"},{"id": "Oroville"},{"id": "Hansboro"},
+    {"id": "Ferry"},{"id": "Tecate"},{"id": "Eastport"},
+    {"id":"Walhalla"},
+    {"id":'Roma'}, {"id":'Naco'}, {"id":'Boquillas'}, {"id":'Raymond'}, {"id":'Porthill'}, {"id":'Norton'},
+    {"id":'Sault Sainte Marie'}, {"id":'Antler'}, {"id":'Rio Grande City'}, {"id":'Del Rio'},
+    {"id":'Sasabe'}, {"id":'Highgate Springs-Alburg'}, {"id":'Del Bonita'}, {"id":'Houlton'},
+    {"id":'Metaline Falls'}, {"id":'Fort Fairfield'}, {"id":'Noonan'}, {"id":'Westhope'}, {"id":'Blaine'},
+    {"id":'Douglas'}, {"id":'Port Huron'}, {"id":'Bridgewater'}, {"id":'Santa Teresa'},
+    {"id":'Lukeville'}, {"id":'Roseau'}, {"id":'Skagway'}, {"id":'Fortuna'}, {"id":'Frontier'},
+    {"id":'Baudette'}, {"id":'Fort Kent'}, {"id":'Grand Portage'}, {"id":'Buffalo-Niagara Falls'},
+    {"id":'Calexico'}, {"id":'Pinecreek'}, {"id":'Northgate'}, {"id":'Sherwood'}, {"id":'Laurier'},
+    {"id":'Detroit'}, {"id":'Jackman'}, {"id":'Ambrose'}, {"id":'Turner'}, {"id":'Dunseith'}, {"id":'Boundary'},
+    {"id":'Sumas'}, {"id":'Alexandria Bay'}, {"id":'Eagle Pass'}, {"id":'Carbury'}, {"id":'Sweetgrass'},
+    {"id":'Piegan'}, {"id":'Tornillo-Fabens'}, {"id":'El Paso'}, {"id":'Port Angeles'}, {"id":'San Luis'},
+    {"id":'Danville'}, {"id":'Nighthawk'}, {"id":'Warroad'}, {"id":'Whitlash'}, {"id":'Maida'},
+    {"id":'Limestone'}, {"id":'Andrade'}, {"id":'Hidalgo'}, {"id":'Richford'}, {"id":'Point Roberts'},
+    {"id":'International Falls-Ranier'}, {"id":'St. John'}, {"id":'Columbus'},
+    {"id":'Brownsville'}, {"id":'Presidio'}, {"id":'Ogdensburg'}, {"id":'Anacortes'}, {"id":'Morgan'},
+    {"id":'Willow Creek'}, {"id":'Roosville'}, {"id":'Laredo'}, {"id":'Hannah'}, {"id":'Ketchikan'},
+    {"id":'Alcan'}, {"id":'Dalton Cache'}, {"id":'Cross Border Xpress'}, {"id":'Friday Harbor'},
+    {"id":'Algonac'}, {"id":'Portland'}, {"id":'Cape Vincent'}, {"id":'Toledo-Sandusky'},
+    {"id":'Whitetail'}, {"id":'Noyes'}, {"id":'Bar Harbor'}]
+    for(let i=0;i<tmp.length;i++){
+        let a = tmp[i]['id'].split(" ")
+        if(a.length > 1){
+            tmp[i]['id'] = a[0] + '_' + a[1]
+        }
+    }
+let relation = []
+d3.csv('./train2019.csv', function(d){
+    let a = d['port'].split(" ")
+    if(a.length > 1)
+        d['port'] = a[0] + '_' + a[1]
 
+    a = d['state'].split(" ")
+    if(a.length > 1)
+        d['state'] = a[0] + '_' + a[1] 
+
+    let re = {"source": d['port'], "target": d['state'], "value": d['value']}
+    relation.push(re)
+    console.log(re)
+});
 d3.json('./data.json').then(function(data){
 
     /* ----------------------------配置参数------------------------  */
